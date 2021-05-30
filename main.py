2ddcb981beb6837e960c9fe55099a9ebe6e95e6d
Kamal Rathi <kamalrathi049@gmail.com>
2021-05-30 14:52:58 +0530

    day3 pip-package-installer

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import utility
-import shoping.shoping_cart as shop
+import pyjokes
 
-import random
-
-print(random.random())
-print(random.randint(1, 10))
-
-if __name__ == '__main__':
-    fruits = shop.my_func('apple', 'banana', 'pineapple', 'mango')
-    num = utility.my_func(100)
-    print(random.choice(fruits))
-
-
-
+joke = pyjokes.get_joke('en', 'neutral')
+print(joke)

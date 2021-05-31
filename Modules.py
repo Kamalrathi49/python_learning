273105a8eeb11737a3ffbe6d7f5d861d7c0c943c
Kamal Rathi <kamalrathi049@gmail.com>
2021-05-31 22:27:27 +0530

    day4 debugging

--- Modules.py
+++ Modules.py
@@ -2,10 +2,10 @@ from collections import Counter, defaultdict, OrderedDict
 import emoji
 
 
-# li = [1, 2, 3, 3, 4, 5, 6, 6, 7, 5]
-# string = emoji.emojize('python is :red_heart: with pycharm :red_heart:')
-# print(Counter(li))
-# print(Counter(string))
+li = [1, 2, 3, 3, 4, 5, 6, 6, 7, 5]
+string = emoji.emojize('python is :red_heart: with pycharm :red_heart:')
+print(Counter(li))
+print(Counter(string))
 
 my_dict = defaultdict(lambda: 0, {
     'a': 1,

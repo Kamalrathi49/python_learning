314901ddb150e5853b71b82f463edcc53d5150d2
Kamal Rathi <kamalrathi049@gmail.com>
2021-05-31 13:59:21 +0530

    day4 modules

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from collections import Counter, defaultdict, OrderedDict
-import emoji
+import utility
+import shoping.shoping_cart as shop
 
-
-li = [1, 2, 3, 3, 4, 5, 6, 6, 7, 5]
-string = emoji.emojize('python is :red_heart: with pycharm :red_heart:')
-print(Counter(li))
-print(Counter(string))
+print(utility.my_func(50))
+print(shop.my_func('apple', 'coffee', 'tea', 'mango'))

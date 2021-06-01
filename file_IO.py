12c74eb37fd3674e080ca3fcea47c74b4c333ffe
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-01 13:02:51 +0530

    day5 file I/O

--- file_IO.py
+++ file_IO.py
@@ -1,3 +1,7 @@
-my_file = open('text_file.txt')
+# with open('text_file.txt', mode='a') as my_file:
+#     text = my_file.write('python is easy to learn :)')
+#     print(my_file.readlines())
 
-print(my_file.readline())
+with open('text.txt', mode='w') as file:
+    texts = file.write('python is pretty complicated :(')
+    print(texts)

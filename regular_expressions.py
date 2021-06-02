c5c9b1b7f63873bd5ced10fc55b178695cb9f8ec
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-02 17:48:02 +0530

    day6 regEx

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,6 +1,5 @@
 import re
 
-
 pattern = re.compile('[h?is]')
 my_str = 'this is regular 1 44 4 2123 444 78 expression'
 a = pattern.findall(my_str)
@@ -9,6 +8,15 @@ c = pattern.split(my_str)
 print(a)
 
 pattern2 = re.compile('^(([\w ]+) ([\d ]+))')
-new_str = "may 25 2020"
-regex = pattern2.findall(new_str)
+my_str2 = "may 25 2020"
+regex = pattern2.findall(my_str2)
 print(regex)
+
+pattern3 = re.compile('^(([\d]+)[^/A-Za-z]([\d]+))')
+my_str3 = "250x300"
+regex2 = pattern3.search(my_str3)
+print(regex2)
+
+
+
+

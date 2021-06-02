961e8c8f767dc070c9b3ad1ae8eab6e0dae12ede
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-02 14:19:24 +0530

    day6 regular expressions

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,6 +1,10 @@
 import re
 
-mt_str = 'this is regular expression'
-a = re.search('this', mt_str)
-print(a)
+pattern = re.compile('is')
+my_str = 'this is regular expression'
+a = pattern.search( my_str)
+b = pattern.split(my_str)
+c = pattern.findall(my_str)
+print(b)
+
 

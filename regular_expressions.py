d3285b3a07ac10cd66c9fc90c3e86d20c36c2ca8
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-02 17:12:00 +0530

    regEx solution

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,10 +1,14 @@
 import re
 
-pattern = re.compile('is')
-my_str = 'this is regular expression'
-a = pattern.search( my_str)
-b = pattern.split(my_str)
-c = pattern.findall(my_str)
-print(b)
+#
+# pattern = re.compile('[h?is]')
+# my_str = 'this is regular 1 44 4 2123 444 78 expression'
+# a = pattern.findall(my_str)
+# b = pattern.search(my_str)
+# c = pattern.split(my_str)
+# print(a)
 
-
+pattern2 = re.compile('^(([\w ]+) ([\d ]+))')
+new_str = "may 25 2020"
+regex = pattern2.findall(new_str)
+print(regex)

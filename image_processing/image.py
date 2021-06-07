e57b7ca7c66327da328033e01fcef328068620fc
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-07 16:29:45 +0530

    day7 image processing

--- image_processing/image.py
+++ image_processing/image.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFilter
 
 img = Image.open("./images/bulbasaur.jpg")
 
-filtered_img = img.filter(ImageFilter.CONTOUR)
-filtered_img.save("contour.png", "png")
+filtered_img = img.convert("L")
+filtered_img.save("converted_img.png", "png")
 
 print(filtered_img)

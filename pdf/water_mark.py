4b2889713847f3aed21b3b315ab72bdae7dee111
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-09 00:05:23 +0530

    day8 PYPDF2

--- pdf/water_mark.py
+++ pdf/water_mark.py
@@ -1,5 +1,13 @@
 import PyPDF2
 
-with open("dummy.pdf", "rb") as dummy_file:
-    file = PyPDF2.PdfFileReader(dummy_file)
-    print(file)
+with open("dummy.pdf", "rb") as dummy_file, open("wtr.pdf", "rb") as watermark_file:
+    template = PyPDF2.PdfFileReader(dummy_file)
+    watermark = PyPDF2.PdfFileReader(watermark_file)
+    output = PyPDF2.PdfFileWriter()
+
+    page = template.getPage(0)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open("merged_file.pdf", "wb") as file:
+        output.write(file)

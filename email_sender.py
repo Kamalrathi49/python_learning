37d704f8478b0808516a2c7e8e6e7fe4c01d690f
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-09 12:58:51 +0530

    sending email from Email module

--- email_sender.py
+++ email_sender.py
@@ -4,13 +4,13 @@ from email.message import EmailMessage
 
 email = EmailMessage()
 email.set_content("this email is dent from python")
-email["From"] = "Your name"
+email["From"] = "sender's Name"
 email["To"] = "receiver's email"
 email["Subject"] = "Email from python"
 
 with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login("your email", "password")
+    smtp.login("sender's email", "password")
     smtp.send_message(email)
     print("done")

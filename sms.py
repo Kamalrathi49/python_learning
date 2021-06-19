96aa3d27e58cf04181335d8b58c3145225a8c036
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-19 14:45:30 +0530

    day11 sending sms with python

--- sms.py
+++ sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
 account_sid = 'account sid'
-auth_token = '[auth token]'
+auth_token = '[]'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     messaging_service_sid='MG945ef48934ee3dbe696cdc728bc399fc',
     body='testing sms in python --by Kamal Rathi',
-    to='+918059910473'
+    to='mobile number'
 )
 
 print(message.sid)

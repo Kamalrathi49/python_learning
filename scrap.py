cb15f067cfbb065014c1a262f2d522d1e7b3f907
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-21 13:03:17 +0530

    day13 data scraping

--- scrap.py
+++ scrap.py
@@ -3,9 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://news.ycombinator.com/"
-
 res = requests.get(url)
-
 soup = BeautifulSoup(res.text, "html.parser")
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
 
-print(soup.select('.score'))
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        hn.append(title)
+
+    return hn
+
+
+create_custom_hn(links, subtext)

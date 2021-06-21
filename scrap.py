69e4497d75d489638ba4b89f49bf86520efe2c7c
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-21 22:06:41 +0530

    added 2nd page of hackernews in the dict

--- scrap.py
+++ scrap.py
@@ -2,15 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
-url = "https://news.ycombinator.com/"
-res = requests.get(url)
+res = requests.get("https://news.ycombinator.com")
+res2 = requests.get("https://news.ycombinator.com/news?p=2")
 soup = BeautifulSoup(res.text, "html.parser")
+soup2 = BeautifulSoup(res2.text, "html.parser")
 links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
 subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+mergedlinks = links + links2
+mergedsubtext = subtext + subtext2
 
 
 def sorted_stories(hnlist):
-    return sorted(hnlist, key=lambda k: k['votes'])
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
@@ -27,4 +33,4 @@ def create_custom_hn(links, subtext):
     return sorted_stories(hn)
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mergedlinks, mergedsubtext))

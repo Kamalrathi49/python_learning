7b04fd7586def24708e25e7f4f01f2b10772fe27
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-29 23:11:32 +0530

    automation with selenium

--- automation/automation.py
+++ automation/automation.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 chrome_path = r"C:\Users\Kamal Rathi\PycharmProjects\my_pythonProject\automation\chromedriver.exe"
 
 driver = webdriver.Chrome(chrome_path)
-driver.get('http://www.google.com/')
+driver.get('https://www.seleniumeasy.com/test/')
+
+print("Selenium Easy - Best Demo website to practice Selenium Webdriver" in driver.title)

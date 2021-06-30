5744090bb4d6d649a3edb9384d346a7ec6470a44
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-30 13:18:37 +0530

    filling input forms using selenium bots

--- automation/automation.py
+++ automation/automation.py
@@ -3,6 +3,15 @@ from selenium import webdriver
 chrome_path = r"C:\Users\Kamal Rathi\PycharmProjects\my_pythonProject\automation\chromedriver.exe"
 
 driver = webdriver.Chrome(chrome_path)
-driver.get('https://www.seleniumeasy.com/test/')
+driver.maximize_window()
+driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
-print("Selenium Easy - Best Demo website to practice Selenium Webdriver" in driver.title)
+
+sm_button = driver.find_element_by_class_name('btn-default')
+
+user_message = driver.find_element_by_id("user-message")
+user_message.clear()
+user_message.send_keys("helllooooooo")
+
+sm_button.click()
+

972d1b056c255e4a52f3e5b4512b4fc9997a80d5
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-30 14:31:18 +0530

    log in to instagram using selenium bots

--- automation/automation.py
+++ automation/automation.py
@@ -1,20 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 chrome_path = r"C:\Users\Kamal Rathi\PycharmProjects\my_pythonProject\automation\chromedriver.exe"
 
 driver = webdriver.Chrome(chrome_path)
 driver.maximize_window()
-driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
-
-sm_button = driver.find_element_by_class_name('btn-default')
-
-user_message = driver.find_element_by_id("user-message")
-user_message.clear()
-user_message.send_keys("helllooooooo")
-
-sm_button.click()
-
-message = driver.find_element_by_id("display")
-print(message.get_attribute("innerHTML"))
-
+sleep(2)
+driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+sleep(3)
+username = driver.find_element_by_name('username')
+username.send_keys('username')
+password = driver.find_element_by_name('password')
+password.send_keys('password')
+submit = driver.find_element_by_tag_name('form')
+submit.submit()
+sleep(4)
+# notify_btn = driver.find_element_by_class_name('aOOlW')
+# notify_btn.click()
+# sleep(2)
+messenger_btn = driver.find_element_by_class_name('Fifk5')
+messenger_btn.click()
